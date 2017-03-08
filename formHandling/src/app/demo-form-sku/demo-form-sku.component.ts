a6f61056522d745a95fe78733a776b723ed18fe1
envy7 <noel.varghese.22@gmail.com>
2017-03-08 21:38:15 +0530

    form handling simple example

---
+++ formHandling/src/app/demo-form-sku/demo-form-sku.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-sku',
+  templateUrl: './demo-form-sku.component.html',
+  styleUrls: ['./demo-form-sku.component.css']
+})
+export class DemoFormSkuComponent {
+	onSubmit(form: any): void{
+		console.log('you submitted value:', form);
+	}
+}
