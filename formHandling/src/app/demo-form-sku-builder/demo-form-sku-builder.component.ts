b7ed192927f763cf25df5a34a33f61b90aaa3e66
envy7 <noel.varghese.22@gmail.com>
2017-03-08 23:02:19 +0530

    added custom form validation

--- formHandling/src/app/demo-form-sku-builder/demo-form-sku-builder.component.ts
+++ formHandling/src/app/demo-form-sku-builder/demo-form-sku-builder.component.ts
@@ -1,25 +1,34 @@
 import { Component} from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-demo-form-sku-builder',
   templateUrl: './demo-form-sku-builder.component.html',
   styleUrls: ['./demo-form-sku-builder.component.css']
 })
+
+
+
 export class DemoFormSkuBuilderComponent{
   myForm: FormGroup;
   sku: AbstractControl;
 
   constructor(fb: FormBuilder) { 
   	this.myForm = fb.group({
-  		'sku': ['', Validators.required]
+  		'sku': ['', Validators.compose([Validators.required, this.skuValidator])]
   	});
 
   	this.sku = this.myForm.controls['sku'];
+
   }
 
   onSubmit(value: string): void{
   	console.log('you submitted value', value);
   }
 
+  skuValidator(control: FormControl): { [s: string]: boolean } {
+	if (!control.value.match(/^123/)) {
+		return {invalidSku: true};
+	}
+  }
 }
