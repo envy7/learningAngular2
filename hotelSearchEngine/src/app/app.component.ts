3b3280fca2516ae4c3fec6b9a0551566f0440019
envy7 <noel.varghese.22@gmail.com>
2017-03-28 23:44:09 +0530

    display profile pic of logged in user on nav bar

--- hotelSearchEngine/src/app/app.component.ts
+++ hotelSearchEngine/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  loggedIn : boolean;
+  userData : Object = null;
+
+   constructor(private af: AngularFire, private auth: AuthService) {
+
+   }
+
+  ngOnInit() {
+    this.af.auth.subscribe(auth => {
+      if(auth){
+        this.loggedIn = true;
+        this.setDetails(auth);
+      }
+      else{
+        this.loggedIn = false;
+        console.log("not logged in");
+      }
+    })
+  }
+
+  setDetails(authObj) {
+    this.userData = {
+      "name" : authObj.auth.providerData[0].displayName,
+      "email" : authObj.auth.providerData[0].email,
+      "photoUrl" : authObj.auth.providerData[0].photoURL,
+      "uid" : authObj.auth.providerData[0].uid
+    }  
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
 }
 
