314679babaa4c596692298bd021647160105b874
envy7 <noel.varghese.22@gmail.com>
2017-04-02 19:29:30 +0530

    add booking for corresponding user in db done

--- hotelSearchEngine/src/app/booking/booking.component.ts
+++ hotelSearchEngine/src/app/booking/booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AppComponent } from '../app.component'
 
 @Component({
   selector: 'app-booking',
@@ -7,12 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
   hotelDetails: Object;
+  items: FirebaseListObservable<any>;
+  userData;
 
-  constructor() { }
+  constructor(private af: AngularFire, public appData: AppComponent) {
+    this.userData = this.appData.userData;
+    console.log(this.userData);
+    this.items = af.database.list(`users/${this.userData.uid}/bookings`);
+  }
 
   ngOnInit() {
     this.hotelDetails = JSON.parse(localStorage.getItem("hotelObj"));
     console.log(this.hotelDetails);
   }
 
+  pay(hotelDetails): void {
+    this.items.push({
+      "hotel" : hotelDetails.name,
+      "price" : hotelDetails.cost
+    })
+  }
 }
